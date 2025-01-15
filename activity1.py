6a99946a06cbcf508668826d2afcba5892e33904
Aliza <59551300+sabeeka131@users.noreply.github.com>
2025-01-15 18:09:46 +0500

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,6 @@
+def func(a,b): #define a function
+    return(a+b) #return sum of both the value
+
+
+sum = func(5,5)
+print(sum) #display the result
---
+++ activity3.py
@@ -0,0 +1,11 @@
+#take user input
+a = input ("Enter a word: ")
+#program to check keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1
+    #display result
+      print ("A is found")
+      break #break statement
+    else:
+    #display result
+      print ("A not found")
